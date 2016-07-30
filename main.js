ad39ad87e80cadc08449a1446d9bc2964971c438
AlyxMoon <allisterkmoon@gmail.com>
2016-07-29 22:29:59 -0700

    Basic info gathering/showing
    Can pull mod list info and display.

--- main.js
+++ main.js
@@ -5,13 +5,24 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 function createWindow () {
     mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.setMenu(null);
 
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 
     mainWindow.on('closed', function () {
         mainWindow = null;
-    })
+    });
+
+    mainWindow.webContents.on('did-finish-load', getCurrentModConfig);
+
+}
+
+function getCurrentModConfig() {
+    var file = require('fs');
+    var filepath = app.getPath('appData') + '\\Factorio\\mods\\mod-list.json';
+
+    mainWindow.webContents.openDevTools();
+    file.readFile(filepath, 'utf8', function(err, data) {
+    });
 }
 
 app.on('ready', createWindow);
