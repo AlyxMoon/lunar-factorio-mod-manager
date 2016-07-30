e7354dc2c3415723a74c246fe300b63b15e5e784
AlyxMoon <allisterkmoon@gmail.com>
2016-07-30 00:22:21 -0700

    Refined search and saving of mod list
    Also includes some basic logging, because why not?

--- main.js
+++ main.js
@@ -1,8 +1,56 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-
 let mainWindow;
+
+function log(data) {
+    var file = require('fs');
+    file.appendFileSync('lunarsModManager_log.txt', data + '\n');
+
+    mainWindow.webContents.send('ping', data);
+}
+
+function getCurrentModConfig() {
+    var file = require('fs');
+    var filepath = app.getPath('appData') + '\\Factorio\\mods\\mod-list.json';
+
+    mainWindow.webContents.openDevTools();
+    try {
+        var data = file.readFileSync(filepath, 'utf8');
+        saveProfile(JSON.parse(data));
+    }
+    catch(error) {
+        if (error.code === 'ENOENT') {
+            log('Not able to find the Factorio Mod List.');
+        }
+    }
+
+
+}
+
+function saveProfile(profile) {
+    var file = require('fs');
+
+    var obj = {};
+    try {
+        var data = file.readFileSync('profiles.json', file.F_OK);
+        obj = JSON.parse(data);
+    }
+    catch(error) {
+        if (error.code === 'ENOENT') {
+            obj['profiles'] = [];
+        }
+        else {
+            log('Unknown error loading profiles.json file: ' + error.code);
+        }
+    }
+
+
+    obj['profiles'].push({'current': profile});
+    file.appendFileSync('profiles.json', JSON.stringify(obj));
+}
+
+
 function createWindow () {
     mainWindow = new BrowserWindow({width: 800, height: 600});
 
@@ -16,14 +64,7 @@ function createWindow () {
 
 }
 
-function getCurrentModConfig() {
-    var file = require('fs');
-    var filepath = app.getPath('appData') + '\\Factorio\\mods\\mod-list.json';
 
-    mainWindow.webContents.openDevTools();
-    file.readFile(filepath, 'utf8', function(err, data) {
-    });
-}
 
 app.on('ready', createWindow);
 
