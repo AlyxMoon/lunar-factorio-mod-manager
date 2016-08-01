93ce3a89e6ac7c8236495ebed7bb7b29d3b8dc1b
AlyxMoon <allisterkmoon@gmail.com>
2016-08-01 00:39:44 -0700

    Cleanup of old work
    I don't like the code I made before. Therefore, it goes!

--- main.js
+++ main.js
@@ -10,52 +10,6 @@ function log(data) {
     mainWindow.webContents.send('ping', data);
 }
 
-function getCurrentModConfig() {
-    var file = require('fs');
-    var filepath = app.getPath('appData') + '\\Factorio\\mods\\mod-list.json';
-
-    mainWindow.webContents.openDevTools();
-    try {
-        var data = file.readFileSync(filepath, 'utf8');
-        profiles = [{
-            'mods': JSON.parse(data)['mods'],
-            'name': 'Current Profile'
-        }];
-
-        mainWindow.webContents.send('data', profiles);
-        //saveProfile(JSON.parse(data));
-    }
-    catch(error) {
-        if (error.code === 'ENOENT') {
-            log('Not able to find the Factorio Mod List.');
-        }
-    }
-
-
-}
-
-function saveProfile(profile) {
-    var file = require('fs');
-
-    var obj = {};
-    try {
-        var data = file.readFileSync('profiles.json', file.F_OK);
-        obj = JSON.parse(data);
-    }
-    catch(error) {
-        if (error.code === 'ENOENT') {
-            obj['profiles'] = [];
-        }
-        else {
-            log('Unknown error loading profiles.json file: ' + error.code);
-        }
-    }
-
-
-    //obj['profiles'].push({'current': profile});
-    //file.appendFileSync('profiles.json', JSON.stringify(obj));
-}
-
 function createWindow () {
     screenSize = electron.screen.getPrimaryDisplay().workAreaSize;
 
@@ -85,11 +39,7 @@ function createWindow () {
         mainWindow = null;
     });
 
-    //mainWindow.webContents.on('did-finish-load', getCurrentModConfig);
-
 }
-
-
 
 app.on('ready', createWindow);
 
