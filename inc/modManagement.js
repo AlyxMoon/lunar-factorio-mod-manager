832990a9407c73bf6413b78f1e379dcf1d7384f8
AlyxMoon <allisterkmoon@gmail.com>
2016-08-14 12:25:53 -0700

    Moved function to module
    Moved getFactorioModList out of main js file.

--- inc/modManagement.js
+++ inc/modManagement.js
@@ -2,6 +2,14 @@ const helpers = require('./helpers.js');
 
 module.exports = {
 
+    getFactorioModList: function(filePath) {
+        helpers.log('Checking for mod list at path: ' + filePath);
+        let file = require('fs');
+
+        let data = file.readFileSync(filePath, 'utf8');
+        return JSON.parse(data)['mods'];
+    },
+
     showInstalledMods: function(window, mods) {
         window.webContents.send('dataInstalledMods', mods);
     }
