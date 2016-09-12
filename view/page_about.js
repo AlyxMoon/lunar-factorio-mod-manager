7915260e73d1e120d79389c4e7f15b22a587578a
Allister Moon <allisterkmoon@gmail.com>
2016-09-12 13:26:35 -0700

    Flesh out the about page

--- view/page_about.js
+++ view/page_about.js
@@ -1,11 +1,26 @@
 const messager = require('electron').ipcRenderer;
-
+const shell = require('electron').shell;
 //---------------------------------------------------------
 // Event listeners for client and server events
+messager.on('dataAppVersion', function(event, version, latestVersion) {
+    showAppVersion(version, latestVersion);
+});
 
-
+$('a').click( function(event) {
+    event.preventDefault();
+    let link = $(this).attr('href');
+    shell.openExternal(link);
+});
 //---------------------------------------------------------
 //---------------------------------------------------------
 $(document).ready(function() {
-
+    messager.send('requestAppVersion');
 });
+
+function showAppVersion(version, latestVersion) {
+    $('#app-version').text(version);
+    if(latestVersion !== undefined) {
+        // TODO: Version checking
+    }
+
+}
