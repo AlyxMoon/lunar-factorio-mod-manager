0ab2c56c93412b08d1c8612f8d9b2b17aaae4614
Daan Vanoverloop <vanoverloopdaan@gmail.com>
2017-04-02 11:07:53 +0200

    electron-json-storage integration

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ let modManager
 // Event listeners for application-related messages
 
 app.on('ready', init)
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') {
     appManager.closeProgram(app, profileManager)
   }
 })
-app.on('activate', function () {
+app.on('activate', function() {
   if (mainWindow === null) {
     mainWindow = appManager.createWindow()
   }
@@ -38,12 +38,12 @@ app.on('activate', function () {
 
 // ------------------------------
 // Handled by AppManager
-appMessager.on('requestAppConfig', function (event) {
+appMessager.on('requestAppConfig', function(event) {
   logger.log(0, 'Event called: requestAppConfig')
   event.sender.send('dataAppConfig', appManager.getAppConfig())
 })
 
-appMessager.on('requestAppVersionInfo', function (event) {
+appMessager.on('requestAppVersionInfo', function(event) {
   logger.log(0, 'Event called: requestAppVersionInfo')
 
   let currentVersion = appManager.getCurrentAppVersion()
@@ -53,7 +53,7 @@ appMessager.on('requestAppVersionInfo', function (event) {
   event.sender.send('dataAppVersionInfo', currentVersion, latestVersion, versionLink)
 })
 
-appMessager.on('startGame', function (event) {
+appMessager.on('startGame', function(event) {
   try {
     appManager.startGame(app, profileManager)
   } catch (error) {
@@ -62,23 +62,23 @@ appMessager.on('startGame', function (event) {
   }
 })
 
-appMessager.on('updateConfig', function (event, data) {
+appMessager.on('updateConfig', function(event, data) {
   appManager.config = data
 })
 
 // ------------------------------
 // Handled by ProfileManager
-appMessager.on('requestAllProfiles', function (event) {
+appMessager.on('requestAllProfiles', function(event) {
   logger.log(0, 'Event called: requestAllProfiles')
   event.sender.send('dataAllProfiles', profileManager.getAllProfiles())
 })
 
-appMessager.on('requestActiveProfile', function (event) {
+appMessager.on('requestActiveProfile', function(event) {
   logger.log(0, 'Event called: requestActiveProfile')
   event.sender.send('dataActiveProfile', profileManager.getActiveProfile())
 })
 
-appMessager.on('newProfile', function (event) {
+appMessager.on('newProfile', function(event) {
   try {
     profileManager.createProfile(modManager.getInstalledModNames())
     event.sender.send('dataAllProfiles', profileManager.getAllProfiles())
@@ -88,7 +88,7 @@ appMessager.on('newProfile', function (event) {
   }
 })
 
-appMessager.on('activateProfile', function (event, index) {
+appMessager.on('activateProfile', function(event, index) {
   try {
     profileManager.activateProfile(index)
   } catch (error) {
@@ -97,7 +97,7 @@ appMessager.on('activateProfile', function (event, index) {
   }
 })
 
-appMessager.on('renameProfile', function (event, index, newName) {
+appMessager.on('renameProfile', function(event, index, newName) {
   try {
     profileManager.renameProfile(index, newName)
     event.sender.send('dataAllProfiles', profileManager.getAllProfiles())
@@ -108,7 +108,7 @@ appMessager.on('renameProfile', function (event, index, newName) {
   }
 })
 
-appMessager.on('deleteProfile', function (event, index) {
+appMessager.on('deleteProfile', function(event, index) {
   try {
     profileManager.deleteProfile(index, modManager.getInstalledModNames())
     event.sender.send('dataAllProfiles', profileManager.getAllProfiles())
@@ -119,7 +119,7 @@ appMessager.on('deleteProfile', function (event, index) {
   }
 })
 
-appMessager.on('sortProfile', function (event, index, direction) {
+appMessager.on('sortProfile', function(event, index, direction) {
   try {
     profileManager.moveProfile(index, direction)
     event.sender.send('dataAllProfiles', profileManager.getAllProfiles())
@@ -129,7 +129,7 @@ appMessager.on('sortProfile', function (event, index, direction) {
   }
 })
 
-appMessager.on('toggleMod', function (event, modName) {
+appMessager.on('toggleMod', function(event, modName) {
   try {
     profileManager.toggleMod(modName)
   } catch (error) {
@@ -140,32 +140,32 @@ appMessager.on('toggleMod', function (event, modName) {
 
 // ------------------------------
 // Handled by ModManager
-appMessager.on('requestInstalledMods', function (event) {
+appMessager.on('requestInstalledMods', function(event) {
   logger.log(0, 'Event called: requestInstalledMods')
   if (modManager) event.sender.send('dataInstalledMods', modManager.getInstalledMods())
 })
 
-appMessager.on('requestOnlineMods', function (event) {
+appMessager.on('requestOnlineMods', function(event) {
   logger.log(0, 'Event called: requestOnlineMods')
   if (modManager) event.sender.send('dataOnlineMods', modManager.getOnlineMods())
 })
 
-appMessager.on('requestModFetchStatus', function (event) {
+appMessager.on('requestModFetchStatus', function(event) {
   logger.log(0, 'Event called: requestModFetchStatus')
   if (modManager) event.sender.send('dataModFetchStatus', modManager.areOnlineModsFetched(), modManager.getOnlineModFetchedCount(), modManager.getOnlineModCount())
 })
 
-appMessager.on('requestPlayerInfo', function (event) {
+appMessager.on('requestPlayerInfo', function(event) {
   logger.log(0, 'Event called: requestPlayerInfo')
   if (modManager) event.sender.send('dataPlayerInfo', modManager.getPlayerUsername())
 })
 
-appMessager.on('requestFactorioVersion', function (event) {
+appMessager.on('requestFactorioVersion', function(event) {
   logger.log(0, 'Event called: requestFactorioVersion')
   if (modManager) event.sender.send('dataFactorioVersion', modManager.getFactorioVersion())
 })
 
-appMessager.on('requestDownload', function (event, modID, modName) {
+appMessager.on('requestDownload', function(event, modID, modName) {
   try {
     manageModDownload(modID, modName)
   } catch (error) {
@@ -173,8 +173,8 @@ appMessager.on('requestDownload', function (event, modID, modName) {
   }
 })
 
-appMessager.on('deleteMod', function (event, index) {
-  modManager.deleteMod(index, function () {
+appMessager.on('deleteMod', function(event, index) {
+  modManager.deleteMod(index, function() {
     event.sender.send('dataInstalledMods', modManager.getInstalledMods())
     profileManager.removeDeletedMods(modManager.getInstalledModNames())
   })
@@ -184,74 +184,76 @@ appMessager.on('deleteMod', function (event, index) {
 // ---------------------------------------------------------
 // Application management functions
 
-function init () {
+function init() {
   let screenSize = electron.screen.getPrimaryDisplay().workAreaSize
-  let configDir = path.join(`${__dirname}`, `data`);
-
-  if (!fs.existsSync(configDir)){
-      fs.mkdirSync(configDir);
-  }
 
   try {
-    appManager = new AppManager(path.join(configDir, `lmm_config.json`))
+    appManager = new AppManager()
   } catch (error) {
     logger.log(4, `Error initializating App Manager. Error: ${error.message}`)
     app.exit(-1)
   }
 
-  let config = appManager.loadConfig(electron, screenSize.width, screenSize.height)
-  if (!config) app.exit(-1)
-
-  try {
-    let baseModPath = path.join(config.game_path, '..', '..', '..', 'data', 'base')
-    modManager = new ModManager(config.modlist_path, config.mod_directory_path, baseModPath, config.player_data_path)
-  } catch (error) {
-    logger.log(4, `Error creating Mod Manager class. Error: ${error.stack}`)
-    app.exit(-1)
-  }
-
-  modManager.loadInstalledMods((err) => {
-    if (err) {
-      logger.log(4, 'Error when loading installed mods')
-      app.exit(-1)
-    }
-
-    logger.log(1, 'Installed mods are loaded.')
-    try {
-      profileManager = new ProfileManager(path.join(`${__dirname}`, `data`, `lmm_profiles.json`), config.modlist_path)
-    } catch (error) {
-      logger.log(4, `Error creating Profile Manager class. Error: ${error.message}`)
-      app.exit(-1)
-    }
+  appManager.loadConfig(electron, screenSize.width, screenSize.height).then((config) => {
+    if (!config) app.exit(-1)
 
     try {
-      mainWindow = appManager.createWindow(screenSize.width, screenSize.height)
+      let baseModPath = path.join(config.game_path, '..', '..', '..', 'data', 'base')
+      modManager = new ModManager(config.modlist_path, config.mod_directory_path, baseModPath, config.player_data_path)
     } catch (error) {
-      logger.log(4, `Error creating the window. Error: ${error.message}`)
+      logger.log(4, `Error creating Mod Manager class. Error: ${error.stack}`)
       app.exit(-1)
     }
 
-    mainWindow.on('resize', function (event) {
-      let newSize = mainWindow.getSize()
-      appManager.config.width = newSize[0]
-      appManager.config.height = newSize[1]
-    })
-    mainWindow.on('move', function (event) {
-      let newLoc = mainWindow.getPosition()
-      appManager.config.x_loc = newLoc[0]
-      appManager.config.y_loc = newLoc[1]
+    modManager.loadInstalledMods((err) => {
+      if (err) {
+        logger.log(4, 'Error when loading installed mods')
+        app.exit(-1)
+      }
+
+      logger.log(1, 'Installed mods are loaded.')
+      try {
+        profileManager = new ProfileManager(config.modlist_path)
+      } catch (error) {
+        logger.log(4, `Error creating Profile Manager class. Error: ${error.message}`)
+        app.exit(-1)
+      }
+
+      profileManager.loadProfiles().then((data) => {
+        try {
+          mainWindow = appManager.createWindow(screenSize.width, screenSize.height)
+        } catch (error) {
+          logger.log(4, `Error creating the window. Error: ${error.message}`)
+          app.exit(-1)
+        }
+
+        mainWindow.on('resize', function(event) {
+          let newSize = mainWindow.getSize()
+          appManager.config.width = newSize[0]
+          appManager.config.height = newSize[1]
+        })
+        mainWindow.on('move', function(event) {
+          let newLoc = mainWindow.getPosition()
+          appManager.config.x_loc = newLoc[0]
+          appManager.config.y_loc = newLoc[1]
+        })
+
+        profileManager.updateProfilesWithNewMods(modManager.getInstalledModNames())
+        profileManager.removeDeletedMods(modManager.getInstalledModNames())
+        mainWindow.loadURL(`file://${__dirname}/view/index.html`)
+      }).catch((error) => {
+        logger.log(4, `Unhandled error saving config file. Error: ${error.message}`)
+      })
     })
 
-    profileManager.updateProfilesWithNewMods(modManager.getInstalledModNames())
-    profileManager.removeDeletedMods(modManager.getInstalledModNames())
-    mainWindow.loadURL(`file://${__dirname}/view/index.html`)
+    modManager.loadPlayerData()
+    modManager.fetchOnlineMods()
+  }).catch((error) => {
+    logger.log(4, `Unhandled error saving config file. Error: ${error.message}`)
   })
-
-  modManager.loadPlayerData()
-  modManager.fetchOnlineMods()
 }
 
-function manageModDownload (modID, modLink) {
+function manageModDownload(modID, modLink) {
   logger.log(1, `Attempting to download mod, link: ${modLink}`)
   modManager.getDownloadInfo(modID, modLink, (error, downloadLink, modName, modIndex) => {
     if (error) logger.log(2, 'Error attempting to download a mod', error)
