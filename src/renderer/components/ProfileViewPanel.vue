9ad8d1d83c71f214177b3c4ec72ee313ea607efb
Allister <allisterkmoon@gmail.com>
2019-11-07 06:12:07 -0800

    Add count of mods in current profile

--- src/renderer/components/ProfileViewPanel.vue
+++ src/renderer/components/ProfileViewPanel.vue
@@ -40,43 +40,51 @@
       </div>
     </div>
 
-    <table v-if="profiles && activeProfile >= 0">
-      <thead>
-        <tr>
-          <th class="cell-check" />
-          <th>Name</th>
-          <th>Version</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="mod in profiles[activeProfile].mods"
-          :class="{ selected: mod.name === selectedMod.name }"
-        >
-          <td class="cell-check">
-            <button
-              @click="removeModFromCurrentProfile(mod)"
-              :disabled="mod.name === 'base'"
-              class="btn red"
-            >
-              <i class="fa fa-minus" />
-            </button>
-          </td>
-          <td @click="selectInstalledMod(mod.name)">
-            <button
-              v-if="isModMissingDependenciesInActiveProfile(mod.name)"
-              @click="addMissingModDependenciesToActiveProfile(mod.name)"
-              class="btn green"
-              title="A required dependency is not included in the profile, click here to add any missing dependencies"
-            >
-              <i class="fa fa-exclamation-circle" />
-            </button>
-            {{ mod.title }}
-          </td>
-          <td>{{ mod.version }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="table-responsive-wrapper">
+      <table v-if="profiles && activeProfile >= 0">
+        <thead>
+          <tr>
+            <th class="cell-check" />
+            <th>Name</th>
+            <th>Version</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="mod in profiles[activeProfile].mods"
+            :class="{ selected: mod.name === selectedMod.name }"
+          >
+            <td class="cell-check">
+              <button
+                @click="removeModFromCurrentProfile(mod)"
+                :disabled="mod.name === 'base'"
+                class="btn red"
+              >
+                <i class="fa fa-minus" />
+              </button>
+            </td>
+            <td @click="selectInstalledMod(mod.name)">
+              <button
+                v-if="isModMissingDependenciesInActiveProfile(mod.name)"
+                @click="addMissingModDependenciesToActiveProfile(mod.name)"
+                class="btn small"
+                title="A required dependency is not included in the profile, click here to add any missing dependencies"
+              >
+                <i class="fa fa-exclamation-circle" />
+              </button>
+              {{ mod.title }}
+            </td>
+            <td>{{ mod.version }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="menu bottom">
+      <div class="menu-section">
+        <span class="menu-label">Mods: {{ profiles[activeProfile].mods.length }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -107,7 +115,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
- .profile-view-panel {
-     overflow-y: auto;
+
+ .menu {
+   height: 40px;
+
+   &.bottom {
+     border-top: 2px solid $highlight-color;
+   }
+ }
+ .table-responsive-wrapper {
+   height: calc(100% - 80px);
+   overflow-y: auto;
  }
 </style>
